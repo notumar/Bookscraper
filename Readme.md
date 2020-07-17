a84187293c78ee7cde29a87d97d72b42db25a1c3
Nukeace <ahmedtijaniumar@gmail.com>
2020-07-17 12:45:31 +0100

    Initial commit with required files

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# BookScraper
+A simple Python webscraper that scrapes books from [To Scrape](http://books.toscrape.com) and saves the titles, price and ratings in a SQLite database.
+
+## Dependencies
+- [x] Beautiful Soup
+- [x] SQLite3
+- [x] Python
+
+## Usage
+- Step 1: Clone this GitHub repository
+  ```bash
+  git clone https://github.com/NukeAce/Bookscraper.git
+  ```
+
+- Step 2: Install *Beautiful Soup 4*   
+  ```bash
+  pip install bs4
+  ```
+- Step2: Run the main file
+  ```bash
+  python3 main.py
+  ```
+- Step3: Check directory for *library.db* database file
+
+
+***Note: Make sure to have Python 3 installed***
---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+
+def scrape_books(url):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    books = soup.findAll("article")
+
+    booklist = []
+    for book in books:
+        # # title = book.find("h3").find("a")["title"]
+        # price = float(book.select(".price_color")[0].get_text().strip("Â £"))
+        # # rating = book.find("div").findNextSibling()["class"][-1]
+
+        title = get_title(book)
+        price = get_price(book)
+        rating = get_rating(book)
+        book_data = (title, price, rating)
+
+        booklist.append(book_data)
+
+    save_books(booklist)
+
+
+def save_books(all_books)
+
+
+connection = sqlite3.connect("library.db/;'")
+c = connection.cursor()
+c.execute('''CREATE TABLE books (title TEXT, price REAL, rating INTEGER)''')
+c.executemany(
+    "INSERT INTO books VALUES(?,?,?)", all_books)
+connection.commit()
+connection.close()
+
+
+def get_title(book):
+    return book.find("h3").find("a")["title"]
+
+
+def get_price(book):
+    return float(book.select(".price_color")[0].get_text().strip("Â £"))
+
+
+def get_rating(book):
+    ratings = {"One": 1, "Two": 2, "Three": 3,
+               "Four": 4, "Five": 5}
+    rating = book.find("div").findNextSibling()["class"][-1]
+    return ratings[rating]
+
+
+scrape_books(
+    "http://books.toscrape.com/catalogue/category/books/history_32/index.html")
